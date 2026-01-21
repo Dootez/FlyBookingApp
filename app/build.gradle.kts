a04168b006713a911788bae7219638a161ec0059
Dootez <eguallartaguado@gmail.com>
2026-01-21 13:39:43 +0100

    Se implementa la principal screen. Errors -> En el button aparece un fondo morado.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.eneko.flybookingapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.eneko.flybookingapp"
--- app/src/main/java/com/eneko/flybookingapp/activties/splash/SplashActivity.kt
+++ app/src/main/java/com/eneko/flybookingapp/activties/splash/SplashActivity.kt
@@ -1,14 +1,102 @@
 package com.eneko.flybookingapp.activties.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.window.SplashScreen
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.eneko.flybookingapp.MainActivity
 import com.eneko.flybookingapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            SplashScreen(onGetStardedClick = {
+                startActivity(Intent(this, MainActivity::class.java))
+            })
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun SplashScreen(onGetStardedClick: () -> Unit = {}) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        ConstraintLayout() {
+            val(backgroundImg, title, subtitle, startbtn) = createRefs()
+            Image(
+                painter = painterResource(R.drawable.splash_bg),
+                contentDescription = null,
+                modifier = Modifier
+                    .constrainAs(backgroundImg) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    .fillMaxSize()
+            )
+            val styledText = buildAnnotatedString {
+                append("Discover your\nDream")
+                withStyle(style = SpanStyle(color = colorResource(R.color.orange))){
+                    append(" Flight")
+                }
+                append("\nEasily")
+            }
+            Text(
+                text = styledText,
+                fontSize = 53.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(top = 32.dp)
+                    .padding(horizontal = 16.dp)
+                    .constrainAs(title) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+            )
+            Text(
+                text = stringResource(R.string.subtitle_splash),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.orange),
+                modifier = Modifier
+                    .padding(top = 16.dp, start = 16.dp)
+                    .constrainAs(subtitle){
+                        top.linkTo(title.bottom)
+                        start.linkTo(title.start)
+                    }
+            )
+            Box(modifier = Modifier.constrainAs(startbtn){
+                bottom.linkTo(parent.bottom)
+            }){
+                GradientButton(onClick = onGetStardedClick, text = "Get Started", padding = 32)
+            }
+        }
     }
 }
