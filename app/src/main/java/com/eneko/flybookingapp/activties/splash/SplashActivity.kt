4c728bcbc20bbe9d755d18ea6807cfd3bc517160
Dootez <eguallartaguado@gmail.com>
2026-01-21 10:45:55 +0100

    Initial commit

---
+++ app/src/main/java/com/eneko/flybookingapp/activties/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.eneko.flybookingapp.activties.splash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.eneko.flybookingapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
